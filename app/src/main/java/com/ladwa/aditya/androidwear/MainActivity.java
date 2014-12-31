bbc2f11696dee68bdb63698b9cf0b179bd2a90eb
LadwaAditya <ladwa.aditya@gmail.com>
2014-12-31 15:26:25 +0530

    Added wearable Notifications

--- app/src/main/java/com/ladwa/aditya/androidwear/MainActivity.java
+++ app/src/main/java/com/ladwa/aditya/androidwear/MainActivity.java
@@ -1,17 +1,31 @@
 package com.ladwa.aditya.androidwear;
 
-import android.support.v7.app.ActionBarActivity;
+import android.app.PendingIntent;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v4.app.NotificationCompat;
+import android.support.v4.app.NotificationManagerCompat;
+import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 
 
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends ActionBarActivity implements View.OnClickListener {
+    private Button button;
+    private int notificationid = 001, eventId = 100;
+    public static String EXTRA_EVENT_ID = "extraeventid";
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        button = (Button) findViewById(R.id.my_button);
+
+        button.setOnClickListener(this);
+
     }
 
 
@@ -35,5 +49,26 @@ public class MainActivity extends ActionBarActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onClick(View v) {
+        Intent viewIntent = new Intent(this, ViewIntent.class);
+        viewIntent.putExtra(EXTRA_EVENT_ID, eventId);
+
+        PendingIntent viewPendingIntent = PendingIntent.getActivity(this, 0, viewIntent, 0);
+
+        NotificationCompat.Builder notificationBuilder = new NotificationCompat.Builder(this)
+                .setSmallIcon(R.drawable.ic_launcher)
+                .setContentTitle("Event Title")
+                .setContentText("Location")
+                .setContentIntent(viewPendingIntent)
+                .addAction(R.drawable.ic_launcher, "Map", viewPendingIntent)
+                .addAction(R.drawable.ic_launcher, "Music", viewPendingIntent);
+
+
+        NotificationManagerCompat notificationManagerCompat = NotificationManagerCompat.from(this);
+        notificationManagerCompat.notify(notificationid, notificationBuilder.build());
+
     }
 }
