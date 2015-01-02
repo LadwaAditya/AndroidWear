a23439de6f3c6484335dfbea7e3809e6af8ae592
LadwaAditya <ladwa.aditya@gmail.com>
2015-01-02 12:27:50 +0530

    Seperate notification for Wear devices

--- myapplication/src/main/java/com/ladwa/aditya/myapplication/MainActivity.java
+++ myapplication/src/main/java/com/ladwa/aditya/myapplication/MainActivity.java
@@ -3,11 +3,15 @@ package com.ladwa.aditya.myapplication;
 import android.app.Activity;
 import android.os.Bundle;
 import android.support.wearable.view.WatchViewStub;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends Activity {
 
     private TextView mTextView;
+    private Button mButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,7 +22,18 @@ public class MainActivity extends Activity {
             @Override
             public void onLayoutInflated(WatchViewStub stub) {
                 mTextView = (TextView) stub.findViewById(R.id.text);
+                mButton = (Button) stub.findViewById(R.id.my_button);
+                mButton.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        Toast.makeText(getApplicationContext(), "You Clicked", Toast.LENGTH_SHORT).show();
+                    }
+                });
+
+
             }
         });
     }
+
+
 }
